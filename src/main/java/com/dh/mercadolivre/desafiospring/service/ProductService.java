0910a7d59049ae9be32b226815165e4014f10719
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-06 15:11:54 -0300

    feat: implements saveProduct method

--- src/main/java/com/dh/mercadolivre/desafiospring/service/ProductService.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/service/ProductService.java
@@ -4,17 +4,23 @@ import com.dh.mercadolivre.desafiospring.dto.ProductDto;
 import com.dh.mercadolivre.desafiospring.model.Product;
 import com.dh.mercadolivre.desafiospring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class ProductService implements IProductService {
 
     @Autowired
     private ProductRepository productRepository;
 
     @Override
-    public ArrayList<ProductDto> saveProduct(ArrayList<Product> productList) {
+    public List<ProductDto> saveProduct(List<Product> productList) {
+        List<Product> updatedProductList = productRepository.saveProduct(productList);
 
-
+        return updatedProductList.stream()
+                .map(ProductDto::new)
+                .collect(Collectors.toList());
     }
 }
