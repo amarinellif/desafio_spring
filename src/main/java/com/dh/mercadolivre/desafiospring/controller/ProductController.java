862aa277bfcabf0e6a2c5c48fbf44c7ee7178476
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-06 15:10:13 -0300

    chore: minor corrections

--- src/main/java/com/dh/mercadolivre/desafiospring/controller/ProductController.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/controller/ProductController.java
@@ -1,25 +1,30 @@
 package com.dh.mercadolivre.desafiospring.controller;
 
+import com.dh.mercadolivre.desafiospring.dto.ProductDto;
 import com.dh.mercadolivre.desafiospring.model.Product;
 import com.dh.mercadolivre.desafiospring.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api/v1")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @PostMapping
-    public void saveProducts(@RequestBody ArrayList<Product> productList) {
+    @PostMapping("/insert-articles-request")
+    public ResponseEntity<List<Product>> saveProducts(@RequestBody List<Product> productList) {
+        List<ProductDto> updatedProductList = productService.saveProduct(productList);
 
+        return new ResponseEntity(updatedProductList, HttpStatus.OK);
     }
 }
 
