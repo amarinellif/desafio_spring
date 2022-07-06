bf0154daa3810ad6af2c5b5de028b7abeb26c4c4
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-06 14:28:40 -0300

    feat: add productRepository bean. create saveProduct method

--- src/main/java/com/dh/mercadolivre/desafiospring/service/ProductService.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/service/ProductService.java
@@ -1,4 +1,20 @@
 package com.dh.mercadolivre.desafiospring.service;
 
-public class ProductService {
+import com.dh.mercadolivre.desafiospring.dto.ProductDto;
+import com.dh.mercadolivre.desafiospring.model.Product;
+import com.dh.mercadolivre.desafiospring.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+public class ProductService implements IProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public ArrayList<ProductDto> saveProduct(ArrayList<Product> productList) {
+
+
+    }
 }
