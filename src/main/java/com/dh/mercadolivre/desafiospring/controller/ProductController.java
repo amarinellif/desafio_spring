af035462c167c68c2bc56db122612c6d46e5f88c
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-06 14:25:59 -0300

    feat: add productService bean

--- src/main/java/com/dh/mercadolivre/desafiospring/controller/ProductController.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/controller/ProductController.java
@@ -1,15 +1,24 @@
 package com.dh.mercadolivre.desafiospring.controller;
 
+import com.dh.mercadolivre.desafiospring.model.Product;
+import com.dh.mercadolivre.desafiospring.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @PostMapping
-    public void saveAllProducts(@RequestBody AllVeiculos allVeiculos) {
+    public void saveProducts(@RequestBody ArrayList<Product> productList) {
 
     }
 }
