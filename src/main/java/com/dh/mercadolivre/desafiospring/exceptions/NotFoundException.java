e5ee6a2808098129f8a45320e1bd0237256da7f8
amarinellif <amanda.marinelli@mercadolivre.com>
2022-07-07 15:58:29 -0300

    R03 Correção do erro NotFoundException

--- src/main/java/com/dh/mercadolivre/desafiospring/exceptions/NotFoundException.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/exceptions/NotFoundException.java
@@ -3,6 +3,7 @@ package com.dh.mercadolivre.desafiospring.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
@@ -10,3 +11,4 @@ public class NotFoundException extends RuntimeException {
         super(message);
     }
 }
+
