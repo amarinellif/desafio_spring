d48252d9e38b7fb755aa7941457d0e8d0104cf8c
amarinellif <amanda.marinelli@mercadolivre.com>
2022-07-07 12:20:12 -0300

    R03 Criação do arquivo para detalhamento das exceções

--- src/main/java/com/dh/mercadolivre/desafiospring/exceptions/NotFoundExceptionDetails.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/exceptions/NotFoundExceptionDetails.java
@@ -1,2 +1,16 @@
-package com.dh.mercadolivre.desafiospring.exceptions;public class NotFoundExceptionDetails {
+package com.dh.mercadolivre.desafiospring.exceptions;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NotFoundExceptionDetails {
+    private String title;
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
 }
