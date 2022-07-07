395cc14c276849c748e6ef7801b95acc50f9416d
racavalcante <rafael.cavalcante@mercadolivre.com>
2022-07-07 09:25:08 -0300

    cria o modelo de uma requisição de compra

---
+++ src/main/java/com/dh/mercadolivre/desafiospring/model/PurchaseRequest.java
@@ -0,0 +1,16 @@
+package com.dh.mercadolivre.desafiospring.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PurchaseRequest {
+    private int productId;
+    private int quantity;
+}
