6b52ca78409bf671340dd3172cc6d5fa1f7dcb45
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-07 16:31:13 -0300

    fix: add city and state attributes

--- src/main/java/com/dh/mercadolivre/desafiospring/dto/ClientDto.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/dto/ClientDto.java
@@ -12,10 +12,14 @@ public class ClientDto {
     private int id;
     private String name;
     private String address;
+    private String city;
+    private String state;
 
     public ClientDto(Client client) {
         this.id = client.getId();
         this.name = client.getName();
         this.address = client.getAddress();
+        this.city = client.getCity();
+        this.state = client.getState();
     }
 }
