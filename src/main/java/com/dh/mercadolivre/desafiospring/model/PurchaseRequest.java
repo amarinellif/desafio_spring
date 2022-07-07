c57c0a04a946de33de214f6b459c3e92d6658baa
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-07-07 17:09:33 -0300

    doc: Inclusão de comentários para documentar classe PurchaseRequest

--- src/main/java/com/dh/mercadolivre/desafiospring/model/PurchaseRequest.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/model/PurchaseRequest.java
@@ -6,10 +6,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Method Getter implemented by Lombok lib for get access the private attributes of the PurchaseRequest Class
+ */
 @Getter
+
+/**
+ * Method Setter implemented by Lombok lib for set the private attributes of the PurchaseRequest Class
+ */
 @Setter
+
+/**
+ * Method Default Constructor implemented by Lombok lib
+ */
 @NoArgsConstructor
+
+/**
+ * Method Constructor of the PurchaseRequest Class
+ */
 @AllArgsConstructor
+
+/**
+ * Class for objects of PurchaseRequest type that contains attributes and methods for create a request
+ * @author Rafael Cavalcante
+ * @version 0.0.1
+ * @see java.lang.Object
+ */
 public class PurchaseRequest {
     private Long productId;
     private Integer quantity;
