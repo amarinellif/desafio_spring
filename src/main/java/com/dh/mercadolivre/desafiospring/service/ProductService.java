b4a043467345ad9102ce085c1ff094b4c3ea7215
diovanavalim <diovana.valim@mercadolivre.com>
2022-07-07 10:49:00 -0300

    chore: remove duplicated method declaration. fix identation

--- src/main/java/com/dh/mercadolivre/desafiospring/service/ProductService.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/service/ProductService.java
@@ -6,7 +6,6 @@ import com.dh.mercadolivre.desafiospring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
