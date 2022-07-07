7d33e9d305ce2803c2a7cd255a98321884b43df7
amarinellif <amanda.marinelli@mercadolivre.com>
2022-07-07 15:55:02 -0300

    R03 Atualização e correção dos arquivos de ExHandler e ExceptionDetails

--- src/main/java/com/dh/mercadolivre/desafiospring/exceptions/ExceptionDetails.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/exceptions/ExceptionDetails.java
@@ -1,6 +1,5 @@
 package com.dh.mercadolivre.desafiospring.exceptions;
 
-
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
-public class NotFoundExceptionDetails {
+public class ExceptionDetails {
     private String title;
     private int status;
     private String message;
