1ed8a58f462ddf309ee30613bf70eab047f84a2c
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-07-08 11:54:58 -0300

    doc: Correção de comentário descritivo da interface

--- src/main/java/com/dh/mercadolivre/desafiospring/service/IProductService.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/service/IProductService.java
@@ -6,35 +6,31 @@ import com.dh.mercadolivre.desafiospring.model.Product;
 import java.util.List;
 
 /**
- * Interface to deal with the methods of the application.
+ * Interface to deal with the methods of the Product class.
  * @author Gabriela Azevedo, Rafael Cavalcante, Diovana Valim, Thiago Frozzi, Amanda Marinelli, Thiago Guimarães
  * @version 0.0.1
  */
 public interface IProductService {
 
     /**
-     *
      * @param category
      * @return
      */
     List<ProductDto> getProductsByCategory(String category);
 
     /**
-     *
      * @param productList
      * @return
      */
     List<ProductDto> saveProductList(List<Product> productList);
 
     /**
-     *
      * @param product
      * @return
      */
     ProductDto saveProduct(Product product);
 
     /**
-     *
      * @param category
      * @param freeShipping
      * @param prestige
