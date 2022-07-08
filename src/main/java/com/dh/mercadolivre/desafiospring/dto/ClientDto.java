6ddc376c948ba8c941fe9550cb592050939ce946
Gabriela Azevedo <gabiazevedoms@gmail.com>
2022-07-08 18:06:35 -0300

    doc: Inclusão de comentário para documentar a classe ClientDto

--- src/main/java/com/dh/mercadolivre/desafiospring/dto/ClientDto.java
+++ src/main/java/com/dh/mercadolivre/desafiospring/dto/ClientDto.java
@@ -6,21 +6,21 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- *
+ * Method Getter implemented by Lombok lib for get access the private attributes of the Client Class
  */
 @Getter
 
 /**
- *
+ * Method Setter implemented by Lombok lib for set the private attributes of the Client Class
  */
 @Setter
 
 /**
- *
+ * Method Default Constructor implemented by Lombok lib.
  */
 @NoArgsConstructor
 /**
- *
+ * Method Constructor of the Client Class.
  */
 public class ClientDto {
     private int id;
@@ -30,8 +30,8 @@ public class ClientDto {
     private String state;
 
     /**
-     *
-     * @param client
+     * Constructor method that receives an object of type Client and returns an ClientDto object
+     * @param client object of type Client.
      */
     public ClientDto(Client client) {
         this.id = client.getId();
